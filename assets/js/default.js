4bd9846ea80e2c5753e4708afe17dcb5a015113f
bulletz-break <91925965+bulletz-break@users.noreply.github.com>
2022-08-18 11:29:42 -0300

    Add files via upload

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,19 +1,27 @@
+let $_content_already_loaded = true;
+
 function $() {
+
     function content_load($_load_file, $_load_elmnt_replace) {
+        // Esperar até que o carregamento anterior termine
+        if(!$_content_already_loaded) return setTimeout(() => { content_load($_load_file, $_load_elmnt_replace); }, 0);
+
+        $_content_already_loaded = false;
         $_cnt_load_request = new XMLHttpRequest();
-        $_cnt_load_request.onloadend = content_loaded($_load_file, $_load_elmnt_replace);
-        $_cnt_load_request.open('post', `../${$_file_load}`, true);
+        $_cnt_load_request.onloadend = () => content_loaded($_cnt_load_request, $_load_elmnt_replace);
+        $_cnt_load_request.open('post', `../${$_load_file}`, true);
         $_cnt_load_request.send();
         return true;
     }
 
     function content_loaded($_cnt_loaded_request, $_loaded_elmnt_replace) {
-        document.querySelector(`#${$_loaded_elmnt_replace}`).innerHTML = $_cnt_loaded_request.reponseText;
+        console.log($_cnt_loaded_request);
+        $_loaded_elmnt_replace.innerHTML = $_cnt_loaded_request.responseText;
+        $_content_already_loaded = true;
         return true;
     }
 
     return {
-        content_load    : content_load,
-        content_loaded  : content_loaded
+        content_load    : content_load
     }
 }
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', () => {
     // Variáveis
     let home_page_header = document.querySelector('#page_header');
-    let home_page_content = document.querySelector('#page_content');
     let home_page_footer = document.querySelector('#page_footer');
 
-    
+    $().content_load('sections/header/top_bar.html', home_page_header);
+
+    $().content_load('sections/footer/rodape.html', home_page_footer);        
 });
