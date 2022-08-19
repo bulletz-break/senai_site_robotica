d909559c62a179ddc0337edb57d542f26f23006a
bulletz-break <91925965+bulletz-break@users.noreply.github.com>
2022-08-19 00:11:50 -0300

    .

---
+++ assets/js/top_bar.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    let top_bar_elmt_logo_image = document.querySelector('#top_bar_logo_image');
+
+    top_bar_elmt_logo_image.setAttribute('src', `${$_base_url}assets/imagens/cybers_logo.svg`);
+});
