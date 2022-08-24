52bc6057f00e7bd7569718158082032efdf010da
bulletz-break <91925965+bulletz-break@users.noreply.github.com>
2022-08-24 06:01:13 -0300

    .

--- assets/js/default.js
+++ assets/js/default.js
@@ -14,7 +14,9 @@ function $() {
         return true;
     }
 
-    function content_loaded($_cnt_loaded_request, $_loaded_elmnt_replace) {
+    function content_loaded($_cnt_loaded_request, $_loaded_elmnt_replace = false) {
+        if(!$_loaded_elmnt_replace) $_loaded_elmnt_replace = document.querySelector('#page_content');
+
         console.log($_cnt_loaded_request);
         $_loaded_elmnt_replace.innerHTML = $_cnt_loaded_request.responseText;
         $_content_already_loaded = true;
@@ -25,3 +27,14 @@ function $() {
         content_load    : content_load
     }
 }
+
+window.addEventListener('load', () => {
+    // Variáveis
+    let home_page_header = document.querySelector('#page_header');
+    let home_page_footer = document.querySelector('#page_footer');
+
+    // Carregando conteúdo padrão
+    $().content_load('sections/header/top_bar.html', home_page_header);
+    $().content_load('sections/footer/rodape.html', home_page_footer);
+    $().content_load('equipe/organizacao-e-metodo.html');
+});
--- assets/js/home.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    // Variáveis
-    let home_page_header = document.querySelector('#page_header');
-    let home_page_footer = document.querySelector('#page_footer');
-
-    $().content_load('sections/header/top_bar.html', home_page_header);
-
-    // $().content_load('sections/footer/rodape.html', home_page_footer);        
-});
